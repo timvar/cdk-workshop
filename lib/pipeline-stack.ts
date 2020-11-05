801ab0f841d5801ce850f67122adb824aa342817
Timo Varila <timo.varila@siili.com>
2020-11-05 10:20:52 +0200

    pipeline w yarn

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,12 +1,34 @@
 import * as cdk from '@aws-cdk/core';
 import * as codecommit from '@aws-cdk/aws-codecommit';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import { SimpleSynthAction, CdkPipeline } from '@aws-cdk/pipelines';
 
 export class WorkshopPipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new codecommit.Repository(this, 'WorkshopRepo', {
+    const repo = new codecommit.Repository(this, 'WorkshopRepo', {
       repositoryName: 'WorkshopRepo',
+    });
+
+    const sourceArtifact = new codepipeline.Artifact();
+
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    new CdkPipeline(this, 'Pipeline', {
+      pipelineName: 'WorkshopPipeline',
+      cloudAssemblyArtifact,
+      sourceAction: new codepipeline_actions.CodeCommitSourceAction({
+        actionName: 'CodeCommit',
+        output: sourceArtifact,
+        repository: repo,
+      }),
+      synthAction: SimpleSynthAction.standardYarnSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+        buildCommand: 'yarn build'
+      })
     });
   }
 }
